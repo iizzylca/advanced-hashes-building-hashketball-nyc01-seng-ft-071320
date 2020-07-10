9870a10dc97d0c469ded38bdd86669d94396404f
Israel Canessa <israellcanessa@gmail.com>
2020-07-10 07:03:45 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,11 +1,21 @@
 hash = {
   :home => [
     :team_name => "Brooklyn Nets",
-    :colors => [
-      "Black",
-      "White"
-    ],
-    :players => [{
+    :colors => ["Black","White"],
+    :players => 
+    [
+      {
+      :player_name =>,
+      :number =>,
+      :shoe =>,
+      :points =>,
+      :rebounds =>,
+      :assists =>,
+      :steals =>,
+      :blocks =>,
+      :slam_dunks =>
+      },
+      {
       :player_name =>,
       :number =>,
       :shoe =>,
@@ -28,38 +38,27 @@ hash = {
       :slam_dunks =>
       },
       {
-          :player_name =>,
-          :number =>,
-          :shoe =>,
-          :points =>,
-          :rebounds =>,
-          :assists =>,
-          :steals =>,
-          :blocks =>,
-          :slam_dunks =>
-          },
-          {
-            :player_name =>,
-            :number =>,
-            :shoe =>,
-            :points =>,
-            :rebounds =>,
-            :assists =>,
-            :steals =>,
-            :blocks =>,
-            :slam_dunks =>
-            },
-            {
-              :player_name =>,
-              :number =>,
-              :shoe =>,
-              :points =>,
-              :rebounds =>,
-              :assists =>,
-              :steals =>,
-              :blocks =>,
-              :slam_dunks =>
-            }
+      :player_name =>,
+      :number =>,
+      :shoe =>,
+      :points =>,
+      :rebounds =>,
+      :assists =>,
+      :steals =>,
+      :blocks =>,
+      :slam_dunks =>
+      },
+      {
+      :player_name =>,
+      :number =>,
+      :shoe =>,
+      :points =>,
+      :rebounds =>,
+      :assists =>,
+      :steals =>,
+      :blocks =>,
+      :slam_dunks =>
+      }
     ]
   ],
   :away => [
